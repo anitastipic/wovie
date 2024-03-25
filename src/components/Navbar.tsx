435102d20c973134d597bd4132d9510a0fcfa742
anitastipic <mailforanitastipic@gmail.com>
2024-03-25 19:21:13 +0100

    adjusted nav to home changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from '../images/logos/WoVie-logo-weiß.png';
-import heroBadge from '../assets/HeroBadge.svg'
+import logo from '../images/logos/WoVie-logo.png';
+import profileIcon from '../../public/profile.png'
 import {Link} from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <nav className="z-20 h-[16vh] w-full px-10 flex items-center fixed justify-between bg-third">
-            <Link to="/" className="flex h-[13vh] w-auto min-w-[20vw] justify-self-start">
+        <nav className="z-20 h-[18%] w-full px-6 py-3 flex items-center fixed justify-between bg-white">
+            <Link to="/" className="flex h-auto w-[14vh] justify-center">
                 <img src={logo} alt="Logo" className="object-contain"/>
             </Link>
             <div className="flex justify-center items-center h-[13vh]">
-                <div className="flex justify-end w-auto">
+                <div className="flex justify-end w-auto text-third">
                     <Link to="/map"
-                          className="text-amber-50 text-lg transition-colors duration-300 transform hover:text-wovie mx-3"
+                          className=" text-lg font-semibold transition-colors duration-300 transform hover:text-wovie mx-3"
                     >Karte</Link>
-                    <Link to="/recycling-1-0-1" className="text-amber-50 text-lg transition-colors duration-300 transform hover:text-wovie mx-3"
+                    <Link to="/recycling-1-0-1" className=" font-semibold text-lg transition-colors duration-300 transform hover:text-wovie mx-3"
                        >Recycling 1 - 0 - 1</Link>
+                    <Link to="/recycling-1-0-1" className=" font-semibold text-lg transition-colors duration-300 transform hover:text-wovie mx-3"
+                    >Über uns</Link>
+                    <Link to="/recycling-1-0-1" className=" font-semibold text-lg transition-colors duration-300 transform hover:text-wovie mx-3"
+                    >Kontakt</Link>
                 </div>
-                <Link to="/hero" className=" h-[6.5vh] flex w-auto">
-                    <img src={heroBadge} alt="Hero" className="object-contain"/>
+                <Link to="/hero" className=" h-[6.5vh] flex w-auto ml-7">
+                    <img src={profileIcon} alt="Hero" className="object-contain"/>
                 </Link>
             </div>
         </nav>
