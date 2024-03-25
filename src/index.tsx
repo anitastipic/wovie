ce4051c551833fb8bd65e9607885429f15f08eb7
anitastipic <mailforanitastipic@gmail.com>
2024-03-25 01:54:18 +0100

    changed to hashrouter for github pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "./pages/Layout.js";
 import ErrorPage from "./pages/ErrorPage.js";
@@ -9,49 +9,25 @@ import MapPage from "./pages/MapPage.tsx";
 import UserSignUp from "./components/UserSignUp.tsx";
 import UserLogin from "./components/UserLogin.tsx";
 import UserPage from "./pages/UserPage.tsx";
-import {AuthProvider} from "./context/AuthenticationContext.tsx";
+import { AuthProvider } from "./context/AuthenticationContext.tsx";
 import InProgress from "./pages/InProgress.tsx";
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Layout/>,
-        errorElement: <ErrorPage/>,
-        children: [
-            {
-                path: "/",
-                element: <Home/>,
-            },
-            {
-                path: "/map",
-                element: <MapPage/>,
-            },
-            {
-                path: "/login",
-                element: <UserLogin/>
-            },
-            {
-                path: 'register',
-                element: <UserSignUp/>
-            },
-            {
-                path: "/hero",
-                element: <UserPage/>
-            },
-            {
-                path: "/recycling-1-0-1",
-                element: <InProgress/>
-            },
-
-        ],
-    },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-    <AuthProvider>
-        <React.StrictMode>
-            <RouterProvider router={router}/>
-        </React.StrictMode>
-    </AuthProvider>
+    <React.StrictMode>
+        <AuthProvider>
+            <HashRouter>
+                <Routes>
+                    <Route path="/" element={<Layout/>} errorElement={<ErrorPage/>}>
+                        <Route index element={<Home/>} />
+                        <Route path="map" element={<MapPage/>} />
+                        <Route path="login" element={<UserLogin/>} />
+                        <Route path="register" element={<UserSignUp/>} />
+                        <Route path="hero" element={<UserPage/>} />
+                        <Route path="recycling-1-0-1" element={<InProgress/>} />
+                    </Route>
+                </Routes>
+            </HashRouter>
+        </AuthProvider>
+    </React.StrictMode>
 );
