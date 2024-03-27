54ac39d297ec8cd23a72a9c47b5955020451d3ca
anitastipic <mailforanitastipic@gmail.com>
2024-03-27 01:12:00 +0100

    add css curvy bg and adjusted responsiveness

---
+++ src/components/Curve.tsx
@@ -0,0 +1,9 @@
+import "./curve.css";
+export default function Curve() {
+
+    return(
+        <section>
+            <div className="curve"></div>
+        </section>
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,61 +1,38 @@
 import Navbar from "../components/Navbar.js";
-import wave from "../assets/wavyBackground.svg";
 import pin from "../assets/pin.svg";
-import wheel from "../assets/wheel.svg";
 import {useNavigate} from "react-router-dom";
 import trashcanYellow from '/trashcanYellow.png';
-import {useEffect, useState} from "react";
-import HomeMobile from "./HomeMobile.tsx";
+import Curve from "../components/Curve.tsx";
 
 
 export default function Home() {
 
     const nav = useNavigate();
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowWidth(window.innerWidth);
-        };
-
-
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
-    const isMobileOrTablet = windowWidth < 1025;
-
     return (
         <>
-
-            <div className="bg-white h-[100vh] overflow-x-clip">
-                {!isMobileOrTablet &&
-                    <Navbar/>
-                }
-                {isMobileOrTablet && <HomeMobile></HomeMobile>}
-                {!isMobileOrTablet &&
-                    <div className="h-[100vh] mt-[0vh] z-10 relative">
-                        <div className="h-[18vh] xl:h-[0vh] w-full"></div>
-                        <div className="h-[82vh] w-full">
-                            <img alt="wavy orange background"
-                                 className="object-cover w-full h-auto top-[0px] md:top-[0px] lg:top-[0px] xl:top-[0px]" src={wave}/>
+            <div className="overflow-x-clip bg-wovie">
+                <Navbar/>
+                <div className="h-[100vh] z-10 relative bg-wovie">
+                    <Curve></Curve>
+                    <div className="sm:flex-none flex flex-col items-center">
+                        <div
+                            className="w-[35vh] sm:w-[50vh] z-20 absolute top-[18vh] sm:top-[35vh] left-[11vw] sm:left-[-5vw] md:left-[6vw] xl:top-[35vh] flex-col text-third justify-items-center text-center">
+                            <p className="text-shadow-wovie font-bold text-[4.5vh] md:text-[5.5vh] text-center">Finde
+                                Container in deiner Nähe</p>
                         </div>
-                        <div className="absolute top-[38vh] xl:top-[30vh] ml-[12.5vw] flex-col text-third justify-items-center text-center">
-                            <p className="font-bold text-[4vw] w-[30vw] text-center">Finde Container in deiner Nähe</p>
-                            <button
-                                className="mt-[5vh] h-[6vw] -ml-[1vw] w-[15vw] bg-third rounded-full text-[2vw] text-amber-50"
-                                onClick={() => nav('/map')}>Zur Karte
-                            </button>
-                            </div>
-                        <img className="absolute h-[18vw] top-[37vh] xl:top-[22vh] left-[74.3vw]" src={pin}/>
-                        <img className="absolute h-[26vw] top-[60vh] xl:top-[50vh] left-[70.3vw]" src={trashcanYellow}/>
-                        </div>
-                        }
+                        <button
+                            className="shadow-slate-500 shadow-md absolute top-[77vh] left-[30vw] sm:left-[11vw] h-[9vh] sm:h-[6vw] w-[40vw] sm:w-[15vw] bg-third rounded-full text-[2.5vh] sm:text-[2vw] text-amber-50"
+                            onClick={() => nav('/map')}>Zur Karte
+                        </button>
+                        <img className="absolute h-[18vw] top-[25vh] xl:top-[22vh] left-[74.3vw] invisible sm:visible"
+                             src={pin}/>
+                        <img className="absolute h-[26vw] top-[50vh] xl:top-[50vh] left-[70.3vw] invisible sm:visible"
+                             src={trashcanYellow}/>
+                        <img className="visible sm:invisible h-[30vh] z-20 absolute top-[40vh]" src={trashcanYellow}/>
                     </div>
-                    </>
-                    )
-                }
+                </div>
+            </div>
+        </>
+    )
+}
 
